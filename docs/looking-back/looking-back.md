2b5132a8c598c3a2eb876a19ee3030c5361f9a3f
Waasiq Masood <waasiqmasood@gmail.com>
2023-01-22 20:36:36 +0300

    Looking back added

---
+++ docs/looking-back/looking-back.md
@@ -0,0 +1,5 @@
+# Looking Back
+
+I have liked to track the things since I was a child, mentally I used to revisit the previous month and assess whether I had been productive or what kind of events had happened. Usually in those years, time relatively felt passing slowly instead of real fast. I can't actually grasp how fast time is passing by, a year literally feels like nothing. I do not have an idea to why the time is passing by so fast, interesting things do happen now and then but usually mostly the routine is repetitive. 
+
+I would say that due to being busy mostly or indulging in activities time tends to go really fast, but again it's just scary that it's 2023 now, 2016 just feels like yesterday. 
--- src/components/HomepageFeatures/index.tsx
+++ src/components/HomepageFeatures/index.tsx
@@ -11,10 +11,7 @@ export default function HomepageFeatures(): JSX.Element {
           <p>
             This is a collection of my personal thoughts, personal logs, goals and acheivements. I have also included some blog posts which I have written over time.
             The concept of self-quantification and self-improvement is something in which I have been interested in for a long time.
-            
-            I would like to credit a lot to <a href="https://github.com/nikitavoloboev">Nikita</a> as his personal website gave me the idea to continue with this project.
-          </p> 
-          <p>Made with love using <a href="https://docusaurus.io/">Docusaurus</a> </p>
+          </p>
         </div>
       </div>
     </section>
